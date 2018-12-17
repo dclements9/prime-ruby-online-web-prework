22aac2e5249ad0e1806eeb32e3b4f1b467bebb91
dclements <dclements@protonmail.com>
2018-12-17 15:36:24 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,17 +1,9 @@
 def prime?(integer)
-while integer > 2
-
-
-
-
-
-  if integer%3 == 0|| integer%2 == 0
-    return false
-  else
-    return true
-  end
-
-
-
-  end
+    return false if integer < 2
+    (2..integer - 1).each do |x|
+        if (integer % x) == 0
+            return false
+        end
+    end
+    true
 end
