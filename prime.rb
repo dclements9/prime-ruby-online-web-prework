fab7c8a2b79e31e02c74dc7136309558b28e8785
dclements <dclements@protonmail.com>
2018-12-17 15:37:47 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,6 +1,8 @@
 def prime?(integer)
-    return false if integer < 2
-    (2..integer - 1).each do |x|
+    if integer < 2
+      return false
+    else integer_array = (2..integer - 1)
+      integer_array.each do |x|
         if (integer % x) == 0
             return false
         end
