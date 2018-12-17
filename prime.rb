8429b65a440253130b842f814553ffe93ee73635
dclements <dclements@protonmail.com>
2018-12-17 15:57:30 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,13 +1,12 @@
-require 'benchmark'
-
-puts Benchmark.measure{
-  def prime?(integer)
-      return false if integer < 2
-      (2..integer - 1).each do |x|
-          if (integer % x) == 0
-              return false
-          end
-      end
-      true
+def prime?(number)
+  if number < 2
+      return false
   end
-}
+  number_array = (2..number - 1)
+    number_array.each do |x|
+  if (number % x) == 0
+    return false
+    end
+  end
+  true
+end
