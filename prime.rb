8d1716b529d8563c9ecf5577bad0e5b367a0d67b
dclements <dclements@protonmail.com>
2018-12-17 15:57:10 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,13 +1,13 @@
 require 'benchmark'
 
 puts Benchmark.measure{
-def prime?(integer)
- (2..integer - 1).each do |x|
-  if (integer % x) == 0
-   return false
-  else
-   return true
+  def prime?(integer)
+      return false if integer < 2
+      (2..integer - 1).each do |x|
+          if (integer % x) == 0
+              return false
+          end
+      end
+      true
   end
-end
-end
 }
